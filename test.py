0403354c6beb8efd59ca85d5ae5a9f0a791f84b8
thisKK <42506615+thisKK@users.noreply.github.com>
2021-03-04 14:42:11 +0700

    add recognition on video

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ import os
 
 import time
 from elasticsearch import Elasticsearch
+es = Elasticsearch([{'host': 'localhost', 'port': '9200'}])
 
 
 # ---------- read Video --------------------------
@@ -26,12 +27,45 @@ face_detector = RetinaFace(gpu_id=0)
 print("load ArcFace Models done!!")
 arcface_model = ArcFace.loadModel()
 
+def createElasticsearchIndex():
+    mapping = {
+        "mappings": {
+            "properties": {
+                "title_vector": {
+                    "type": "dense_vector",
+                    "dims": 512
+                },
+                "title_name": {"type": "keyword"}
+            }
+        }
+    }
+    # es.indices.create(index="face_recognition", body=mapping)
+
 def addFaceToDatabase():
-    print("hello")
+    createElasticsearchIndex()
+    files = []
+    for r, d, f in os.walk("./tests/dataset/"):
+        for file in f:
+            if file.endswith('.jpg'):
+                files.append(r + file)
+                print(file)
+
+    index = 0
+    for img_path in files:
+        img = functions.preprocess_face(img_path, target_size=(112, 112), detector_backend="retina",
+                                        enforce_detection=False)
+
+        embedding = arcface_model.predict(img)
+        embedding = embedding[0]
+        print(img_path)
+        doc = {"title_name": img_path, "title_vector": embedding}
+
+        es.create("face_recognition", id=index, body=doc)
+
+        index = index + 1
 
 
 def search_face(target_embeddimg):
-    es = Elasticsearch([{'host': 'localhost', 'port': '9200'}])
     query = {
         "size": 1,
         "query": {
@@ -87,7 +121,33 @@ def preprocess_face(img, target_size):
         res_obj.append(result)
     return res_obj
 
-def streamOnCammera(image):
+def streamOnCammera():
+    while True:
+        isSuccess, frame = cap.read()
+        cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        try:
+            t0 = time.time()
+            res_obj = preprocess_face(frame, target_size=(112, 112))
+            for img, box, score in res_obj:
+                embedding = arcface_model.predict(img)
+                embedding = embedding[0]
+                score, source = search_face(embedding)
+                cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), color=(255, 0, 0), thickness=1)
+                cv2.putText(frame, source + str(score), (box[0], box[1] - 5), cv2.FONT_HERSHEY_COMPLEX, 0.7,
+                            (255, 255, 255), 2)
+        except:
+            pass
+        cv2.imshow("", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            print("Terminate by user")
+            break
+        t1 = time.time()
+        print("frame")
+        print(f'took {round(t1 - t0, 3)} to process')
+        # out.write(cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+
+def streamOnVideo(pathVideo):
+    cap = cv2.VideoCapture(pathVideo)
     while True:
         isSuccess, frame = cap.read()
         cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -119,7 +179,9 @@ def processOnImage(img):
 
 
 if __name__ == "__main__":
-    streamOnCammera()
+    # streamOnCammera()
     # img = cv2.imread('./tests/dataset/lisatest.jpg')
     # processOnImage(img)
+    # streamOnVideo('./tests/video/testVideo1.mp4')
+    addFaceToDatabase()
 
